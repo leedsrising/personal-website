9ed894f6800d3aafb49c16d2a0fa78403720260d
Leeds Rising <leedsrising@Leedss-MBP.lan>
2022-02-12 17:47:46 -0500

    :bug: fix import naming

--- src/routes/Home/App.jsx
+++ src/routes/Home/App.jsx
@@ -1,31 +1,9 @@
 import '../../App.css';
 import React from "react";
-import { Link } from "react-router-dom";
 
-import { 
-  FaLinkedin,
-  FaGithubSquare,
-  FaEthereum
-} from "react-icons/fa";
+import TopNav from '../../TopNav.jsx'
 
 import 'react-pro-sidebar/dist/css/styles.css';
-
-function TopNav() {
-  return (
-    <div class="topnav">
-      <div class="topnav-left">
-        <Link to="/home" class="homelink">leedsrising.com</Link>
-        <Link to="/writing">writing</Link>
-        <Link to="/projects">projects</Link>
-      </div>
-      <div class="topnav-right">
-        <a href="https://www.linkedin.com/in/leedsrising/"><FaLinkedin class="fa-linkedin fa-icon"/></a>
-        <a href="https://github.com/leedsrising"><FaGithubSquare class="fa-github fa-icon"/></a>
-        <a href="https://etherscan.com/address/0xce2772143637052abddcad413826c736fc5c03ee"><FaEthereum class="fa-ethereum fa-icon"/></a>
-      </div>
-    </div>
-  )
-}
 
 function Home() {
   return (
