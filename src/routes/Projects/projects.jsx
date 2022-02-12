e62cf4aaa84efe4476cce0d0551b6a349c10d53e
Leeds Rising <leedsrising@Leedss-MBP.lan>
2022-02-12 17:53:14 -0500

    :tada: init /projects and /writing

--- src/routes/Projects/projects.jsx
+++ src/routes/Projects/projects.jsx
@@ -1,9 +1,14 @@
 import '../../App.css';
 
+import TopNav from '../../TopNav.jsx'
+
 export default function Projects() {
     return (
-      <main style={{ padding: "1rem 0" }}>
-        <h2>projects</h2>
-      </main>
+      <div class="Home">
+        <TopNav/>
+        <main style={{ padding: "1rem 0" }}>
+          <h2>projects</h2>
+        </main>
+      </div>
     );
   }
