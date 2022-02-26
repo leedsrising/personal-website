5f82f23e13dc911b5741e76aba9d3a399f00bbfb
Leeds Rising <leedsrising@Leedss-MBP.lan>
2022-02-25 21:45:42 -0500

    :liptstick: bubble css and icon

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -13,8 +13,9 @@ function Home() {
         <p>
           <b>Hello! I'm Leeds</b>
           <div class="description-div description-text">
-            <p class="description-text">I'm a PM interested in programming, philosophy, history, startups, and investing. I'm currently working at <a href="golden.com" class="Bubble-text">Golden</a>.</p>
-            <p class="description-text">I started writing in 2021, and want to continually expand this site with what im learning about software development, design, product, their intersection, and everything in between.</p>
+            <p class="description-text">I'm a PM interested in programming, philosophy, history, startups, and investing. I'm currently working at <div class="Bubble-text"><a href="golden.com" style={{'text-decoration': 'none'}}>Golden</a></div></p>
+            <p class="description-text">I started writing in 2021, and want to continually expand this site with what im learning about software development, design, product, their intersection, and everything in between</p>
+            <p class="description-text">If you're trying to get in touch, feel free to drop me a DM on <div class="Bubble-text"><a href="https://twitter.com/LeedsRising" style={{'text-decoration': 'none'}}>Twitter</a></div></p>
           </div>
         </p>
       </header>
